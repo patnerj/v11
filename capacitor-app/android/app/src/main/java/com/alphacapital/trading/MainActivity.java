91ed1b04c1c935c9b493528f697521fa42365f9e
Muhammad Haris <patnerj95@gmail.com>
2026-09-01 14:50:30 +0500

    Mobile UX enhancements: native back navigation, status bar colors, and floating dock

--- capacitor-app/android/app/src/main/java/com/alphacapital/trading/MainActivity.java
+++ capacitor-app/android/app/src/main/java/com/alphacapital/trading/MainActivity.java
@@ -2,4 +2,13 @@ package com.alphacapital.trading;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+            bridge.getWebView().goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
